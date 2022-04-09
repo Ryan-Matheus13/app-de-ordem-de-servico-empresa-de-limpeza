8f3ad94fa954f9a565f7d881d41a4a82424ef863
Ryan Matheus <ryanmatheusdev@gmail.com>
2022-04-08 23:18:58 -0300

    Criação de usuário e grupo personalizados

--- crud_app/models.py
+++ crud_app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from equipe.models import Funcionario
 
-# Create your models here.
+class Cliente(models.Model):
+    # dados
+    nome_completo = models.CharField(max_length=60)
+    email = models.EmailField()
+    cpf = models.CharField(max_length=11)
+    data_de_nascimento = models.DateField()
+    telefone = models.CharField(max_length=11)
+
+    # Endereço
+    logradouro = models.CharField(max_length=150)
+    numero = models.CharField(max_length=4)
+    bairro = models.CharField(max_length=20)
+    cidade = models.CharField(max_length=20)
+    estado = models.CharField(max_length=20)
+
+    # Registro
+    data_do_registro = models.DateTimeField(auto_now_add=True)
+    registrado_por = models.ForeignKey(Funcionario, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome_completo
+
+class Servico(models.Model):
+    # Dados do serviço
+    nome_do_servico = models.CharField(max_length=60)
+    valor = models.DecimalField(max_digits=6, decimal_places=2)
+    descricao = models.TextField()
+
+    # Registro
+    data_do_registro = models.DateTimeField(auto_now_add=True)
+    registrado_por = models.ForeignKey(Funcionario, on_delete=models.SET_NULL, null=True, blank=True)
+
+class Atendimento(models.Model):
+    SITUACAO_CHOICES = (
+        ('Em andamento', 'Em andamento'),
+        ('Concluido', 'Concluido'),
+        ('Cancelado', 'Cancelado')
+    )
+
+    # Dados do atendimento
+    servico = models.ForeignKey(Servico, on_delete=models.SET_NULL, null=True, blank=True)
+    data_do_servico = models.DateTimeField() 
+    situacao = models.CharField(max_length=12, choices=SITUACAO_CHOICES)
+    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True, blank=True)
+
+    # Registro
+    data_do_registro = models.DateTimeField(auto_now_add=True)
+    registrado_por = models.ForeignKey(Funcionario, on_delete=models.SET_NULL, null=True, blank=True)
---
+++ equipe/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser, Group
+
+class Funcionario(AbstractUser):
+    class Meta:
+        verbose_name = ("funcionario")
+        verbose_name_plural = ("funcionarios")
+        db_table = "funcionarios"
+
+class Cargo(Group):
+    class Meta:
+        verbose_name = ("cargo")
+        verbose_name_plural = ("cargos")
